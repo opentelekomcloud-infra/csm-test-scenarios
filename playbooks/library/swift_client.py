62484c9419a6cbd1852769c778aeccb5c00284e7
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-02-18 15:54:23 +0300

    fix complex function

--- playbooks/library/swift_client.py
+++ playbooks/library/swift_client.py
@@ -63,7 +63,7 @@ EXAMPLES = '''
         var1: 1
         var2: 2
   register: result
-      
+
 - name: Swift list containers
   swift_client:
     state: fetch
@@ -81,7 +81,7 @@ EXAMPLES = '''
     container: csm
     object_name: lb_monitoring_inventory
   register: result
-  
+
 - name: Swift delete object
   swift_client:
     state: absent
@@ -93,7 +93,7 @@ EXAMPLES = '''
   swift_client:
     state: absent
     container: test
-  register: r
+  register: result
 '''
 
 
@@ -124,58 +124,71 @@ class SwiftClient(SwiftModule):
         content=dict(type='str', required=False)
     )
 
+    def present(self, container, object_name=None, content=None):
+        data = []
+        containers = [item['name'] for item in self.client.containers()]
+        if container not in containers:
+            data = self.client.create_container(container)
+            data.pop('location')
+        if content and object_name:
+            if is_path(content):
+                content = read_yaml(content)
+            else:
+                content = content.replace("'", "\"")
+            raw = self.client.create_object(
+                container=container,
+                name=object_name,
+                data=content
+            )
+            dt = raw.to_dict()
+            dt.pop('location')
+            data.append(dt)
+        return data
+
+    def absent(self, container, object_name=None):
+        if object_name:
+            data = self.client.delete_object(
+                container=container,
+                obj=object_name
+            )
+            return data
+        data = self.client.delete_container(container=container)
+        return data
+
+    def fetch(self, container=None, object_name=None):
+        data = []
+        if container and object_name:
+            data = self.client.download_object(object_name, container)
+            return json.loads(data)
+        if container:
+            for raw in self.client.objects(container):
+                dt = raw.to_dict()
+                dt.pop('location')
+                data.append(dt)
+            return data
+        for raw in self.client.containers():
+            dt = raw.to_dict()
+            dt.pop('location')
+            data.append(dt)
+        return data
+
     def run(self):
         changed = False
         container = self.params['container']
         object_name = self.params['object_name']
         state = self.params['state']
         content = self.params['content']
-        data = []
         if state == 'present':
-            containers = [item['name'] for item in self.client.containers()]
-            if container not in containers:
-                data = self.client.create_container(container)
-                data.pop('location')
-                changed = True
-            if content and object_name:
-                if is_path(content):
-                    content = read_yaml(content)
-                else:
-                    content = content.replace("'", "\"")
-                raw = self.client.create_object(
-                    container=container,
-                    name=object_name,
-                    data=content
-                )
-                dt = raw.to_dict()
-                dt.pop('location')
-                data.append(dt)
-                changed = True
+            data = self.present(container, object_name, content)
+            changed = True
             self.exit(changed=changed, data=data)
         if state == 'absent':
-            if object_name:
-                data = self.client.delete_object(
-                    container=container,
-                    obj=object_name
-                )
-                self.exit(changed=changed, deleted_object=data)
-            data = self.client.delete_container(container=container)
-            self.exit(changed=changed, deleted_container=data)
+            data = self.absent(container, object_name)
+            changed = True
+            self.exit(changed=changed, data=data)
         if state == 'fetch':
-            if container and object_name:
-                data = self.client.download_object(object_name, container)
-                self.exit(changed=changed, object=json.loads(data))
-            if container:
-                for raw in self.client.objects(container):
-                    dt = raw.to_dict()
-                    dt.pop('location')
-                    data.append(dt)
-                    self.exit(changed=changed, objects=data)
-            for raw in self.client.containers():
-                dt = raw.to_dict()
-                dt.pop('location')
-                data.append(dt)
-                self.exit(changed=changed, containers=data)
+            data = self.fetch(container, object_name)
+            self.exit(changed=changed, data=data)
 
 
 def main():

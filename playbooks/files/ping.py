3fb486e571b3684357a0364eb742b5bbd3714d0b
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2021-04-12 19:34:24 +0300

    make script universal

--- playbooks/files/ping.py
+++ playbooks/files/ping.py
@@ -17,28 +17,32 @@ def serialize_metric(msg):
 def emit_metric(host, port, metric):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as _socket:
         _socket.connect((host, int(port)))
-        msg = '%s\n' % serialize_metric(metric)
+        msg = f'{serialize_metric(metric)}\n'
         _socket.sendall(msg.encode('utf8'))
 
 
 def main():
-    host = sys.argv[1]
-    metric_name = 'csm_peering_ping.%s' % sys.argv[2]
-    if len(sys.argv) == 4:
-        request_timeout = int(sys.argv[3])
+    host = sys.argv[2]
+    metric_name = f'{sys.argv[1]}.{sys.argv[3]}'
+    if len(sys.argv) == 5:
+        request_timeout = int(sys.argv[4])
     else:
         request_timeout = 5
-    graphite_host = sys.argv[4]
-    graphite_port = sys.argv[5]
+    graphite_host = sys.argv[5]
+    graphite_port = sys.argv[6]
+    if len(sys.argv) == 8:
+        packet_size = sys.argv[7]
+    else:
+        packet_size = '56'
     duration = -1
     try:
         rsp = sp.check_output(
-            ['ping', '-c', '1', host],
+            ['ping', '-c', '1', '-s', packet_size, host],
             stderr=sp.STDOUT,
             universal_newlines=True
         )
         duration = re.search(r'time=(\d+)', rsp).group(1)
-        metric_name = '%s.success' % metric_name
+        metric_name = f'{metric_name}.success'
         metric = dict(
             name=metric_name,
             value=duration,
@@ -48,15 +52,14 @@ def main():
         rc = 0
     except AttributeError as ae:
         metric = dict(
-            name='%s.failed' % metric_name,
+            name=f'{metric_name}.failed',
             metric_type='c',
             __type='metric'
         )
         rc = 3
-        print('%s caused %s by invalid response' % (host, ae))
+        print(f'{host} caused {ae} by invalid response')
 
     emit_metric(graphite_host, graphite_port, metric)
-
     exit(rc)
 
 
